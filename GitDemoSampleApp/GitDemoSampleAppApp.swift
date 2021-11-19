9a7818256560592f4da4aa651f44ce5c2c75e57e
Training2021 <gitrainingrepos@gmail.com>
2021-11-19 13:09:47 +0530

    Initial Commit

---
+++ GitDemoSampleApp/GitDemoSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoSampleAppApp.swift
+//  GitDemoSampleApp
+//
+//  Created by Prabhu Patil on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
